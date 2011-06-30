b7f8b651c6ff5e9b37dca58f87c18f637a76d2f6
James Holt <holtjma@auburn.edu>
2011-06-30 11:41:45 -0400

    made a non-critical fix to simulator

--- AU_UAV_stack/AU_UAV_ROS/src/AU_UAV_ROS/SimulatedPlane.cpp
+++ AU_UAV_stack/AU_UAV_ROS/src/AU_UAV_ROS/SimulatedPlane.cpp
@@ -112,65 +112,69 @@ bool AU_UAV_ROS::SimulatedPlane::fillTelemetryUpdate(AU_UAV_ROS::TelemetryUpdate
 	double x = cos(lat1)*sin(lat2) - sin(lat1)*cos(lat2)*cos(deltaLong);
 	this->bearing = atan2(y, x)*RADIANS_TO_DEGREES;
 	
-	//calculate the real bearing based on our maximum angle change
-	//first create a temporary bearing that is the same as bearing but at a different numerical value
-	double tempBearing;
-	if(this->bearing < 0) tempBearing = this->bearing + 360;
-	else tempBearing = this->bearing - 360;
-	
-	double diff1 = abs(this->actualBearing - this->bearing);
-	double diff2 = abs(this->actualBearing - tempBearing);
-	
-	//check for easy to calculate values first
-	if(diff1 < MAXIMUM_TURNING_ANGLE || diff2 < MAXIMUM_TURNING_ANGLE)
-	{
-		//the difference is less than our maximum angle, set it to the bearing
-		this->actualBearing = this->bearing;
-	}
-	else
-	{
-		//we have a larger difference than we can turn, so turn our maximum
-		double mod;
-		if(diff1 < diff2)
-		{
-			if(this->bearing > this->actualBearing) mod = MAXIMUM_TURNING_ANGLE;
-			else mod = 0 - MAXIMUM_TURNING_ANGLE;
-		}
-		else
-		{
-			if(tempBearing > this->actualBearing) mod = MAXIMUM_TURNING_ANGLE;
-			else mod = 0 - MAXIMUM_TURNING_ANGLE;
-		}
-		
-		//add our mod, either +22.5 or -22.5
-		this->actualBearing = this->actualBearing + mod;
-		
-		//tweak the value to keep it between -180 and 180
-		if(this->actualBearing > 180) this->actualBearing = this->actualBearing - 360;
-		if(this->actualBearing < -180) this->actualBearing = this->actualBearing + 360;
-	}
-	
-	//ROS_INFO("Plane %d has bearing %lf", this->planeID, this->actualBearing);
-	
-	//time to calculate the new positions, God help us
-	/*
-	Algorithm for updating position:
-	1) Estimate new latitude using basic trig and this equation:
-	   lat2 = lat1 + (MPS_SPEED*cos(bearing))*METERS_TO_LATITUDE
-	2) Use law of haversines to find the new longitude
-	   haversin(c) = haversin(a-b) + sin(a)*sin(b)*haversin(C)
-	   where haversin(x) = (sin(x/2.0))^2
-	   where c = MPS_SPEED/EARTH_RADIUS (radians)
-	   where a = 90 - lat1 (degrees)
-	   where b = 90 - lat2 (degrees)
-	   where C = the change in longitude, what we are solving for
-	   
-	   C = 2.0 * arcsin(sqrt((haversin(c) - haversin(a-b))/(sin(a)*sin(b))))
-	*/
-	
 	//make sure we're actually traveling somewhere
 	if(this->currentWaypointIndex >= 0)
 	{
+		//calculate the real bearing based on our maximum angle change
+		//first create a temporary bearing that is the same as bearing but at a different numerical value
+		double tempBearing = -1000;
+		if((this->bearing) < 0)
+		{
+			tempBearing = this->bearing + 360;
+		}
+		else
+		{
+			tempBearing = this->bearing - 360;
+		}
+		
+		double diff1 = abs(this->actualBearing - this->bearing);
+		double diff2 = abs(this->actualBearing - tempBearing);
+	
+		//check for easy to calculate values first
+		if(diff1 < MAXIMUM_TURNING_ANGLE || diff2 < MAXIMUM_TURNING_ANGLE)
+		{
+			//the difference is less than our maximum angle, set it to the bearing
+			this->actualBearing = this->bearing;
+		}
+		else
+		{
+			//we have a larger difference than we can turn, so turn our maximum
+			double mod;
+			if(diff1 < diff2)
+			{
+				if(this->bearing > this->actualBearing) mod = MAXIMUM_TURNING_ANGLE;
+				else mod = 0 - MAXIMUM_TURNING_ANGLE;
+			}
+			else
+			{
+				if(tempBearing > this->actualBearing) mod = MAXIMUM_TURNING_ANGLE;
+				else mod = 0 - MAXIMUM_TURNING_ANGLE;
+			}
+		
+			//add our mod, either +22.5 or -22.5
+			this->actualBearing = this->actualBearing + mod;
+		
+			//tweak the value to keep it between -180 and 180
+			if(this->actualBearing > 180) this->actualBearing = this->actualBearing - 360;
+			if(this->actualBearing <= -180) this->actualBearing = this->actualBearing + 360;
+		}
+	
+		//time to calculate the new positions, God help us
+		/*
+		Algorithm for updating position:
+		1) Estimate new latitude using basic trig and this equation:
+		   lat2 = lat1 + (MPS_SPEED*cos(bearing))*METERS_TO_LATITUDE
+		2) Use law of haversines to find the new longitude
+		   haversin(c) = haversin(a-b) + sin(a)*sin(b)*haversin(C)
+		   where haversin(x) = (sin(x/2.0))^2
+		   where c = MPS_SPEED/EARTH_RADIUS (radians)
+		   where a = 90 - lat1 (degrees)
+		   where b = 90 - lat2 (degrees)
+		   where C = the change in longitude, what we are solving for
+		   
+		   C = 2.0 * arcsin(sqrt((haversin(c) - haversin(a-b))/(sin(a)*sin(b))))
+		*/
+	
 		//1) Estimate new latitude using basic trig and this equation
 		this->currentLocation.latitude = lat1*RADIANS_TO_DEGREES + (MPS_SPEED*cos(this->actualBearing*DEGREES_TO_RADIANS))*METERS_TO_LATITUDE;
 		
